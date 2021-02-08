6559dc2e83fac31d7a4e3e3450e31ebd483884cb
Chelsea Boling <cmboling@github.com>
2021-02-08 12:10:09 -0800

    Update main.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -178,9 +178,20 @@ router.beforeEach((to, from, next) => {
   }
 })
 
+const someTest = new Vue({
+  data: {
+    myProperty: 42
+  },
+  created: () => {
+    // BAD: prints: "myProperty is: undefined"
+    console.log('myProperty is: ' + this.myProperty);
+  }
+});
+
 new Vue({
   render: h => h(App),
   store,
   router,
   jwt
 }).$mount('#app')
+
